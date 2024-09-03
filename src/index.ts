4990ebe8b05d39b006fced1254fc76b468429026
Callum <callum.mcintyre@anza.xyz>
2024-09-03 14:33:22 +0100

    Just return the proxy response directly
    Upstream version messes with the headers, seems unnecessary

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,15 @@ interface Env {
 }
 
 function makeHeliusRpcUrlBase(isDevnet: boolean) {
-	return `${isDevnet ? 'devnet' : 'mainnet'}.helius-rpc.com`
+	return `${isDevnet ? 'devnet' : 'mainnet'}.helius-rpc.com`;
 }
 
 function makeHeliusApiUrlBase(isDevnet: boolean) {
-	return `api${isDevnet ? '-devnet' : ''}.helius.xyz`
+	return `api${isDevnet ? '-devnet' : ''}.helius.xyz`;
 }
 
 export default {
 	async fetch(request: Request, env: Env) {
-
 		// If the request is an OPTIONS request, return a 200 response with permissive CORS headers
 		// This is required for the Helius RPC Proxy to work from the browser and arbitrary origins
 		// If you wish to restrict the origins that can access your Helius RPC Proxy, you can do so by
@@ -24,19 +23,19 @@ export default {
 		// originated from one of the domains in the `CORS_ALLOW_ORIGIN` environment variable.
 		const supportedDomains = env.CORS_ALLOW_ORIGIN ? env.CORS_ALLOW_ORIGIN.split(',') : undefined;
 		const corsHeaders: Record<string, string> = {
-			"Access-Control-Allow-Methods": "GET, HEAD, POST, PUT, OPTIONS",
-			"Access-Control-Allow-Headers": "*",
-		}
+			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, OPTIONS',
+			'Access-Control-Allow-Headers': '*',
+		};
 		if (supportedDomains) {
-			const origin = request.headers.get('Origin')
+			const origin = request.headers.get('Origin');
 			if (origin && supportedDomains.includes(origin)) {
-				corsHeaders['Access-Control-Allow-Origin'] = origin
+				corsHeaders['Access-Control-Allow-Origin'] = origin;
 			}
 		} else {
-			corsHeaders['Access-Control-Allow-Origin'] = '*'
+			corsHeaders['Access-Control-Allow-Origin'] = '*';
 		}
 
-		if (request.method === "OPTIONS") {
+		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 200,
 				headers: corsHeaders,
@@ -47,29 +46,28 @@ export default {
 		const heliusRpcUrlBase = makeHeliusRpcUrlBase(isDevnet);
 		const heliusApiUrlBase = makeHeliusApiUrlBase(isDevnet);
 
-		const upgradeHeader = request.headers.get('Upgrade')
+		const upgradeHeader = request.headers.get('Upgrade');
 
 		if (upgradeHeader || upgradeHeader === 'websocket') {
-			return await fetch(`https://${heliusRpcUrlBase}/?api-key=${env.HELIUS_API_KEY}`, request)
+			return await fetch(`https://${heliusRpcUrlBase}/?api-key=${env.HELIUS_API_KEY}`, request);
 		}
 
-
-		const { pathname, search } = new URL(request.url)
+		const { pathname, search } = new URL(request.url);
 		const payload = await request.text();
-		const proxyRequest = new Request(`https://${pathname === '/' ? heliusRpcUrlBase : heliusApiUrlBase}${pathname}?api-key=${env.HELIUS_API_KEY}${search ? `&${search.slice(1)}` : ''}`, {
-			method: request.method,
-			body: payload || null,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Helius-Cloudflare-Proxy': 'true',
+		const proxyRequest = new Request(
+			`https://${pathname === '/' ? heliusRpcUrlBase : heliusApiUrlBase}${pathname}?api-key=${
+				env.HELIUS_API_KEY
+			}${search ? `&${search.slice(1)}` : ''}`,
+			{
+				method: request.method,
+				body: payload || null,
+				headers: {
+					'Content-Type': 'application/json',
+					'X-Helius-Cloudflare-Proxy': 'true',
+				},
 			}
-		});
+		);
 
-		return await fetch(proxyRequest).then(res => {
-			return new Response(res.body, {
-				status: res.status,
-				headers: corsHeaders
-			});
-		});
+		return await fetch(proxyRequest);
 	},
 };
